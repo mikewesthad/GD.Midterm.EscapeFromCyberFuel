7fb819bc62530e209afb4935f699d13c1d4f4598
GD278 <57197845+GD278@users.noreply.github.com>
2020-03-08 17:06:11 -0500

    Main Menu and Assets Added to DevScene
    Main menu was added and Devscene was updated with more assets

---
+++ GD.Midterm/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private AudioSource source;
+    [SerializeField] private AudioClip hoverOver;
+    [SerializeField] private AudioClip mouseClick;
+    [SerializeField] GameObject camera;
+    void Start()
+    {
+        source = camera.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void OnMouseEnter()
+    {
+        source.PlayOneShot(hoverOver);
+    }
+     public void OnMouseDown()
+    {
+        source.PlayOneShot(mouseClick);
+    }
+}
---
+++ GD.Midterm/Assets/Scripts/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource source;
+    [SerializeField] AudioClip song;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = song;
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
