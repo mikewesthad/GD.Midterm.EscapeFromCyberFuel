7f523afa7fbc00c0e04f0310ed034a71a027436d
Michael Hadley <michaelwesthadley@gmail.com>
2020-03-09 12:06:21 -0500

    Test commit of reorg unity project from subdir to root

---
+++ Assets/Scripts/CollisionTracker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CollisionTracker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] ParticleSystem fire;
+    [SerializeField] Text GameOverText;
+    [SerializeField] Text replayTextBox;
+    PlayerController playerController;
+    Rigidbody playerRb;
+    Timer timer;
+    bool gameOver;
+    private void Awake()
+    {
+        fire.Stop();
+    }
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+        playerRb = GetComponent<Rigidbody>();
+        timer = GetComponent<Timer>();
+        replayTextBox.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == true)
+        {
+            timer.enabled = false;
+            playerRb.useGravity = true;
+            replayTextBox.enabled = true;
+            playerController.enabled = false;
+            GameOverText.text = "Game over!";
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(gameObject.tag == "Player" && collision.gameObject.tag == "Obstacle")
+        {
+            fire.Play();
+            gameOver = true;
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreTracker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text ScoreTextBox;
+    [SerializeField] Text youWinTextBox;
+    [SerializeField] Text replayTextBox;
+    PlayerController playerController;
+    Timer timer;
+    [SerializeField] GameObject[] rings;
+    bool triggered;
+    
+    
+    int i;
+    
+    void Start()
+    {
+        i = 0;
+        triggered = false;
+        playerController = GetComponent<PlayerController>();
+        timer = GetComponent<Timer>();
+        replayTextBox.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        triggered = false;
+        if(i >= rings.Length)
+        {
+            youWinTextBox.text = "You Win!";
+            playerController.enabled = false;
+            timer.enabled = false;
+            replayTextBox.enabled = true;
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Hoop") && !triggered)
+        {
+            triggered = true;
+            Destroy(other.gameObject);
+            Debug.Log("Hoop destroyed.");
+            Debug.Log("You have entered the trigger.");
+            i++;
+            Debug.Log("Score added.");
+            Destroy(other.gameObject);
+            Debug.Log("Trigger destroyed.");
+            ScoreTextBox.text = $"Score: {i}";
+        }
+    }
+}
